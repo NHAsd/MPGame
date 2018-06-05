3bd45cf6228827197cd5494e636f2b42bfeeb44c
Asd <test@myemail.com>
2018-06-05 20:47:51 +0200

    send data

--- src/main/java/MPGame/MPGame/Cli.java
+++ src/main/java/MPGame/MPGame/Cli.java
@@ -1,5 +1,30 @@
 package MPGame.MPGame;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Cli {
 //http://cs.lmu.edu/~ray/notes/javanetexamples/
+	public static void main (String [] args) throws UnknownHostException, IOException {
+
+		Socket s = new Socket("127.0.0.1", 50000);
+//		PrintWriter pw = new PrintWriter(s.getOutputStream());
+//
+//		
+//		pw.println("testy");
+//
+//		pw.flush();
+		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+		dos.writeInt(4);
+		dos.flush();
+
+		
+				
+	}
 }
